dbc1bb78798843525c73b3ab0d16948f30c152f5
Mike Gale <mgale@idtech.com>
2021-06-24 16:30:05 -0400

    GameManager now references the PlayerBase
    GameManager now references the PlayerBase class.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; } = null;
 
-    public PlayerMotor _PlayerMotor = null;
+    public PlayerBase _PlayerCharacter = null;
 
     private void Awake()
     {
