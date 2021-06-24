5afa8342d7ac9d2e0396563b73d374c72dc79ea0
Mike Gale <mgale@idtech.com>
2021-06-24 14:28:11 -0400

    Added Game Manager with reference to player.
    Added Game Manager with reference to player.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; } = null;
+
+    public PlayerMotor _PlayerMotor = null;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogWarning("You put multiple GameManager instances in your scene.  Make sure there is only one.");
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+}
