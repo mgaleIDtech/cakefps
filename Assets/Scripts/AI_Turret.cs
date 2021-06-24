e9200dd27d670aaa17bb950a4516639758d1c452
Mike Gale <mgale@idtech.com>
2021-06-24 17:53:16 -0400

    Update AI_Turret.cs
    Now uses gun.

--- Assets/Scripts/AI_Turret.cs
+++ Assets/Scripts/AI_Turret.cs
@@ -7,10 +7,28 @@ public class AI_Turret : EnemyBase
     [SerializeField]
     private Transform _pivotPoint = null;
 
+    [SerializeField]
+    private Gun _gun = null;
+
+    private const float SEE_DISTANCE = 20f;
+
     private void Update()
+    {
+        ShootAtPlayer();
+        LookForPlayer();
+    }
+
+    private void LookForPlayer()
     {
         var player = GameManager.Instance._PlayerCharacter;
 
         transform.LookAt(player.transform);
+    }
+
+    private void ShootAtPlayer()
+    {
+        var dist = Vector3.Distance(transform.position, GameManager.Instance._PlayerCharacter.transform.position);
+        if (dist <= SEE_DISTANCE)
+            _gun.Shoot();
     }
 }
