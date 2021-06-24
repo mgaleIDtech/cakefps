82234e1e59e6e3b1a56254ea1af6aff70ac8a438
Mike Gale <mgale@idtech.com>
2021-06-24 16:29:10 -0400

    AI_Turret now derives from EnemyBase
    AI_Turret now derives from EnemyBase

--- Assets/Scripts/AI_Turret.cs
+++ Assets/Scripts/AI_Turret.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AI_Turret : MonoBehaviour
+public class AI_Turret : EnemyBase
 {
     [SerializeField]
     private Transform _pivotPoint = null;
 
     private void Update()
     {
-        var player = GameManager.Instance._PlayerMotor;
+        var player = GameManager.Instance._PlayerCharacter;
 
         transform.LookAt(player.transform);
     }
