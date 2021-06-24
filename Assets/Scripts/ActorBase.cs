abc200d964ef0d8393decd4da379ebd2d6077a4f
Mike Gale <mgale@idtech.com>
2021-06-24 16:28:25 -0400

    Added ActorBase and derived classes.
    Added the ActorBase class and the player / enemy derived classes so they can share health functionality while allowing them to create their own individual data.

---
+++ Assets/Scripts/ActorBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorBase : MonoBehaviour
+{
+    public int HealthPoints { get { return _healthPoints; } }
+
+    [SerializeField]
+    protected int _healthPoints = 1;
+
+    public void HurtActor(int hpToRemove)
+    {
+        if (hpToRemove < 0)
+            hpToRemove = 0;
+
+        _healthPoints -= hpToRemove;
+
+        if (_healthPoints <= 0)
+            Kill();
+    }
+
+    public void HealActor(int hpToRemove)
+    {
+        if (hpToRemove < 0)
+            hpToRemove = 0;
+
+        _healthPoints += hpToRemove;
+    }
+
+    public void Kill()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBase : ActorBase
+{
+    private void Start()
+    {
+        GameManager.Instance._PlayerCharacter = this;
+    }
+}
