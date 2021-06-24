9eff63d16f3022ee7ba6f3e284c99c86650280ab
Mike Gale <mgale@idtech.com>
2021-06-24 15:14:04 -0400

    Bullet destroys itself when hitting a collider.
    Bullet destroys itself when hitting a collider.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,11 +5,45 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 2f;
+    private float _speed = 20f;
+
+    private Rigidbody _rb = null;
+
+    private float _ttl = 4f;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
-        var moveVector = transform.TransformDirection(Vector3.forward) * (_speed * Time.deltaTime);
-        //_cc.Move(moveVector);
+        HandleTTL();
+        Move();
+    }
+
+    private void Move()
+    {
+        var moveVector = transform.TransformDirection(Vector3.forward) * (_speed);
+        _rb.velocity = moveVector;
+    }
+
+    private void HandleTTL()
+    {
+        _ttl -= Time.deltaTime;
+
+        if (_ttl <= 0f)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            var enemy = other.gameObject.GetComponent<AI_Turret>();
+            Destroy(other.gameObject);
+        }
+
+        Destroy(gameObject);
     }
 }
