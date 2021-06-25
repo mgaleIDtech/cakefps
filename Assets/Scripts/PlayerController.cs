e2a917b092c8c844480ee938ec1a6f7ea2494c26
Mike Gale <mgale@idtech.com>
2021-06-25 12:34:38 -0400

    Added jumping and code commenting.
    Jumping now works as expected.  Added extensive code commenting to the PlayerMotor and PlayerController class.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,68 +3,116 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Takes input from the player and applies it to their assigned GameObject that has an ActorMotor attached to it.
+/// </summary>
+[DisallowMultipleComponent]
 public class PlayerController : MonoBehaviour
 {
     #region Inspector Variables
 
-
+    /// <summary>
+    /// The ActorMotor component this controller is sending data to.
+    /// </summary>
+    public PlayerMotor TargetMotor = null;
 
     #endregion
 
     #region Local Members
 
-    private PlayerMotor _playerMotor = null;
-    private Gun _playerGun = null;
+    /// <summary>
+    /// The button for jumping.
+    /// </summary>
+    private const KeyCode _jumpButton = KeyCode.Space;
+
+    /// <summary>
+    /// A Vector2 in the scope of the entire class prevents needing to create a new one constantly, helping performance.
+    /// </summary>
+    private Vector2 _moveVector = Vector2.zero;
+
+    /// <summary>
+    /// A deadzone helps prevent player input in the case of a malfunctioning axis.
+    /// Some game controllers may constantly give off a small input on the axis even when not touched.
+    /// </summary>
+    private const float AXIS_DEAD_ZONE = 0.001f;
 
     #endregion
 
-    private void Awake()
+    #region Methods
+
+    /// <summary>
+    /// Checks if there was a valid actor assigned to the controller to prevent null reference errors from crashing code.
+    /// This can be assigned directly in the inspector, or through code.
+    /// </summary>
+    /// <param name="showLogWarnings">Optional parameter than can be set to false if you don't want error messages.</param>
+    /// <returns>True if there is a valid actor assigned.</returns>
+    private bool IsValidActorAssigned(bool showLogWarnings = true)
     {
-        _playerMotor = GetComponent<PlayerMotor>();
-        if (_playerMotor == null)
-            Debug.LogError("Could not find a PlayerMotor component on " + gameObject.name);
-
-
-        _playerGun = GetComponent<Gun>();
-        if (_playerGun == null)
-            Debug.LogError("Could not find a PlayerGun component on " + gameObject.name);
-    }
-
-    private void Update()
-    {
-        var axisValues = new Vector2();
-        axisValues = GetAxisValues();
-        MovePlayer(axisValues);
-        HandleJump();
-        HandleShooting();
-    }
-
-    private Vector2 GetAxisValues()
-    {
-        var axisValues = new Vector2();
-
-        axisValues.x = Input.GetAxis("Horizontal");
-        axisValues.y = Input.GetAxis("Vertical");
-
-        return axisValues;
-    }
-
-    private void MovePlayer(Vector2 axisValues)
-    {
-        _playerMotor.MoveWithJoystick(axisValues);
-    }
-
-    private void HandleJump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Send an error message to the editor so we can be reminded this needs a PlayerMotor.
+        if (TargetMotor == null)
         {
-            _playerMotor.Jump();
+            if (showLogWarnings)
+                Debug.LogWarning("This is not a valid ActorMotor component assigned to the PlayerController.");
+            
+            return false;
+        }
+        else
+        {
+            return true;
         }
     }
 
-    private void HandleShooting()
+    /// <summary>
+    /// Reads the player's joystick axis.  This is WASD on a keyboard, or the left joystick on a game controller.
+    /// This method converts the raw data, which are floats for each axis, and combines it into an easily transferable
+    /// Vector2.  Turning it into a single data structure allows for both XY data to be passed around code easily.
+    /// </summary>
+    /// <param name="axisValues">By using out, we allow the method to alter a pre-allocated variable, allowing for optimization by preventing allocating each check.</param>
+    /// <returns>The X,Y values of the player's joystick axis.</returns>
+    private bool GetAxisValues(out Vector2 axisValues)
     {
-        if (Input.GetMouseButtonDown(0))
-            _playerGun.Shoot();
+        // Read input from the player controller or WASD keys and cache them.
+        axisValues.x = Input.GetAxis("Horizontal");
+        axisValues.y = Input.GetAxis("Vertical");
+
+        // Check if the deadzone requirements are met.
+        return Mathf.Abs(axisValues.x) >= AXIS_DEAD_ZONE || Mathf.Abs(axisValues.y) >= AXIS_DEAD_ZONE;
+    }
+
+    /// <summary>
+    /// This takes in the joystick axis data as a Vector2, and then sends it to the ActorMotor to process that movement.
+    /// </summary>
+    /// <param name="axisValues">The XY values of the game controller or WASD keys.</param>
+    private void HandleMoveInput()
+    {
+        // If there is player input and a valid actor, then tell the motor to move.
+        if (GetAxisValues(out _moveVector))
+        {
+            if (IsValidActorAssigned())
+                TargetMotor.MoveWithJoystick(_moveVector);
+        }
+    }
+
+    /// <summary>
+    /// Sends a jump command to the ActorMotor if the relevant key is pressed.
+    /// </summary>
+    private void HandleJumpInput()
+    {
+        if (Input.GetKeyDown(_jumpButton))
+        {
+            if (IsValidActorAssigned())
+                TargetMotor.Jump();
+        }
+    }
+
+    #endregion
+
+    /// <summary>
+    /// The core logic loop of this component that runs every frame.
+    /// </summary>
+    private void Update()
+    {
+        HandleMoveInput();
+        HandleJumpInput();
     }
 }
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -2,85 +2,219 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The player motor is a reusable component that can be applied to either a player or enemy to handle movement of the CharacterController.
+/// </summary>
+[RequireComponent(typeof(CharacterController))]
+[DisallowMultipleComponent]
 public class PlayerMotor : MonoBehaviour
 {
-    private CharacterController _cc = null;
+    #region Inspector Variables
 
+    /// <summary>
+    /// Movement speed of the actor.
+    /// </summary>
     [SerializeField]
     private float _speed = 8f;
+
+    /// <summary>
+    /// The rotational speed of the actor.
+    /// </summary>
     [SerializeField]
     private float _speedRot = 360f;
+
+    /// <summary>
+    /// The custom gravity applied to this actor.
+    /// </summary>
     [SerializeField]
     private float _gravity = 15f;
-    private const float _maxRiseSpeed = 50f;
+
+    /// <summary>
+    /// The amount of force applied to the initial jump action.
+    /// </summary>
     [SerializeField]
     private float _jumpPower = 8f;
+
+    /// <summary>
+    /// The max number of jumps the actor can perform before landing.
+    /// Note: Less than 1 will disable jumping completely.
+    /// </summary>
+    [SerializeField]
+    private int _numberOfJumps = 2;
+
+    #endregion
+
+    #region Local Members
+
+    /// <summary>
+    /// The cached CharacterController attached to the same object.
+    /// </summary>
+    private CharacterController _cc = null;
+
+    /// <summary>
+    /// A terminal velocity for preventing too much gravity if the actor falls a long time.
+    /// </summary>
     private float _maxFallSpeed { get { return _gravity; } }
+
+    /// <summary>
+    /// An upwards velocity cap for preventing too much gravity if code accidently creates an unrealistic value.
+    /// </summary>
+    private const float _maxRiseSpeed = 50f;
+
+    /// <summary>
+    /// A counter used to make sure the actor only jumps the specified amount of times possible.
+    /// • This is reset to 0 when the actor lands.
+    /// </summary>
+    private int _jumpsUsed = 0;
 
     /// <summary>
     /// The gravity (or anti-gravity) the character is currently being applied with.
     /// </summary>
     private float _currentVertPower = 0f;
 
-    Vector3 _moveVector = Vector3.zero;
+    /// <summary>
+    /// A movement vector that caches the movement commands during Update() so it can be applied in FixedUpdate().
+    ///     • As of Unity v2021, CharacterController.Move() seems to require FixedUpdate() like a Rigidbody would.
+    /// </summary>
+    private Vector3 _moveVector = Vector3.zero;
 
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Awake runs only once for this component.  Use it for initializing and caching values.
+    /// </summary>
     private void Awake()
     {
+        // Cache the CharacterController on the same GameObject.
         _cc = GetComponent<CharacterController>();
 
+        // If no CharacterController: Disable and show descriptive error message in console.
         if (_cc == null)
+        {
+            enabled = false;
             Debug.LogError("Could not find a CharacterController component on " + gameObject.name);
+        }
     }
 
-    private void FixedUpdate()
+    /// <summary>
+    /// Applies the cached movement vector to the CharacterController.
+    /// </summary>
+    private void MoveCharacterController()
     {
-        Debug.Log("_cc.isGrounded: " + _cc.isGrounded);
-        ApplyGravityToMoveVector();
-        ApplyMoveVector();
-    }
-
-    private void ApplyMoveVector()
-    {
-        if (_moveVector.y == 0f)
-            Debug.Log("_moveVector: " + _moveVector);
+        // Moves the actor.
+        //  • Note, this handles the Time.deltaTime, so don't need to use Time.delaTime in other movement calculations.
+        //  • Try to only set .Move once per frame to prevent possible quirky bugs.
         _cc.Move(_moveVector * Time.deltaTime);
+
+        // Reset the movement vector so no data is left over in the next update cycle.
         _moveVector = Vector3.zero;
     }
 
+    /// <summary>
+    /// Calculates gravity effect on vertical motion and caches the value in the movement vector's Y.
+    /// </summary>
     private void ApplyGravityToMoveVector()
     {
+        // If the actor is grounded, and not going up, then...
         if (_cc.isGrounded && _currentVertPower <= 0f)
         {
             // If _moveVector.y ever isn't negative, then isGrounded will be false.
             _currentVertPower = -_gravity * Time.deltaTime;
+
+            // Reset jumps used because the actor is grounded.
+            _jumpsUsed = 0;
         }
         else
         {
+            // This runs when the actor is airborne, either while jumping or falling.
             _currentVertPower -= _gravity * Time.deltaTime;
             _currentVertPower = Mathf.Clamp(_currentVertPower, -_maxFallSpeed, _maxRiseSpeed);
         }
 
+        // Cache the calculated vertical movement direction in the movement vector...
+        //      so it can be applied to the CharacterController in another method.
         _moveVector.y = _currentVertPower;
     }
 
+    /// <summary>
+    /// Checks if the actor can jump based on conditions.
+    /// </summary>
+    /// <returns>True if it can.</returns>
+    private bool CanJump()
+    {
+        return _jumpsUsed < _numberOfJumps;
+    }
+
+    /// <summary>
+    /// Checks if this component is enabled, and if not will throw an error message.
+    /// </summary>
+    /// <param name="postErrorMessage">If true then will post any errors to the console.</param>
+    /// <returns>True if the component is enabled.</returns>
+    private bool IsEnabled(bool postErrorMessage = true)
+    {
+        if (enabled)
+        {
+            return true;
+        }
+        else
+        {
+            Debug.LogWarning(string.Format("ActorMotor attempted to move, but the component is disabled on GameObject: {0}", gameObject.name));
+            return false;
+        }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Interprets a Vector2 as the XY axis on a player joystick and converts it to the proper movement axis for the actor's personal directions.
+    /// </summary>
+    /// <param name="joystickAxis">The XY values of the joystick.</param>
     public void MoveWithJoystick(Vector2 joystickAxis)
     {
-        // Move
-        // Vertical (Y) is forward and backwards.
+        if (!IsEnabled()) return; // Don't execute any code if the component isn't enabled.
+
+        // Move (Vertical (Y) is forward and backwards.)
+        //  • TransformDirection is used to get the forward of this actor, instead of just the forward of world space.
         _moveVector = transform.TransformDirection(Vector3.forward) * (joystickAxis.y * _speed);
 
-        // Rotation
-        // Horizontal (X) is rotating
+        // Rotation (Horizontal (X) is rotating)
+        //  • Unity transforms use Quaternions on the backend, so convert to euler to edit them.
+        //  • Inspector values are actually Euler and not Quaternion.
         var rot = transform.rotation.eulerAngles;
         rot.y += joystickAxis.x * _speedRot * Time.deltaTime;
         transform.rotation = Quaternion.Euler(rot);
     }
 
-    public void Jump()
+    /// <summary>
+    /// When invoked, this method will attempt to jump.
+    /// By default: If the actor is already jumping, this will ignore the jump command.
+    /// </summary>
+    /// <param name="forceJump">Set to true to ignore if the player is jumping already</param>
+    public void Jump(bool forceJump = false)
     {
-        if (_cc.isGrounded)
+        if (!IsEnabled()) return; // Don't execute any code if the component isn't enabled.
+
+        if (CanJump() || forceJump)
         {
-            _currentVertPower = _jumpPower;
+            _jumpsUsed++; // Increment the number of jumps used (resets when the actor lands).
+            _currentVertPower = _jumpPower; // Set the vertical velocity so the player going up.
         }
+    }
+
+    #endregion
+
+    /// <summary>
+    /// The core logic loop of this component that runs every frame.
+    ///     • FixedUpdate() is like Update(), but is used specifically for physics objects.
+    ///     • If FixedUpdate() is not used, you will get erroneous results when using Rigidbody or CharacterController.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        ApplyGravityToMoveVector();
+        MoveCharacterController();
     }
 }
